2b732d95e357dd56179e88ba6ef92255eab43c84
ISTC-CREATE LAB <babujyan.hrach@gmail.com>
2018-07-21 18:12:35 +0400

    last one for today

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -2,35 +2,38 @@ import numpy as np
 
 
 class PCA:
-    def __init__(self, k):
-        self.k = k
-        self.location_ = None
-        self.matrix_ = None
+   def __init__(self, k):
+       self.k = k
+       self.location_ = None
+       self.matrix_ = None
 
-    def fit(self, data):
-        """
-        finds best params for X = Mu + A * Lambda
-        :param data: data of shape (number of samples, number of features)
-        HINT! use SVD
-        """
-        data = data / np.linalg.norm(data, axis=0)
-        mean_ = np.mean(data, axis=0)
-        data -= mean_
-        #         cov_matix = np.cov(data.T.dot(data))
-        #         w , v  = np.linalg.eig(cov_matix)
+   def fit(self, data):
+       """
+       finds best params for X = Mu + A * Lambda
+       :param data: data of shape (number of samples, number of features)
+       HINT! use SVD
+       """
+       #data = data / np.linalg.norm(data, axis=0)
+       mean_ = np.mean(data, axis=0)
+       data -= mean_
+       #         cov_matix = np.cov(data.T.dot(data))
+       #         w , v  = np.linalg.eig(cov_matix)
 
-        u, s, v = np.linalg.svd(data, full_matrices=False)
-        self.matrix_ = v[:self.k]
+       u, s, v = np.linalg.svd(data, full_matrices=False)
+       self.matrix_ = v[:self.k]
 
-    def transform(self, data):
-        """
-        for given data returns Lambdas
-        x_i = mu + A dot lambda_i
-        where mu is location_, A is matrix_ and lambdas are projection of x_i
-        on linear space from A's rows as basis
-        :param data: data of shape (number of samples, number of features)
-        """
-        # Lemma: x is vector and A dot A.T == I, then x's coordinates in Linear Space(A's rows as basis)
-        # is A dot x
+   def transform(self, data):
+       """
+       for given data returns Lambdas
+       x_i = mu + A dot lambda_i
+       where mu is location_, A is matrix_ and lambdas are projection of x_i
+       on linear space from A's rows as basis
+       :param data: data of shape (number of samples, number of features)
+       """
+       # Lemma: x is vector and A dot A.T == I, then x's coordinates in Linear Space(A's rows as basis)
+       # is A dot x
 
-        return np.dot(self.matrix_, data.T).T
+       return np.dot(self.matrix_, data.T).T
+
+   def return_components(self):
+       return self.matrix_
--- PCA/dim_reduce.py
+++ PCA/dim_reduce.py
@@ -9,39 +9,68 @@ Files will work if train-images-idx3-ubyte.gz file and
 train-labels-idx1-ubyte.gz files are in the same directory as this
 python file.
 """
-import PCA
+from __future__ import print_function
+import argparse
+import gzip
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+from PCA import  PCA
 # TODO: Import you PCA
 
 
 def parse_args(*argument_array):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mnist-train-data',
-                        default='train-images-idx3-ubyte.gz',  # noqa
-                        help='Path to train-images-idx3-ubyte.gz file '
-                        'downloaded from http://yann.lecun.com/exdb/mnist/')
-    args = parser.parse_args(*argument_array)
-    return args
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--mnist-train-labels',
+                       default='train-labels-idx1-ubyte.gz',  # noqa
+                       help='Path to train-labels-idx1-ubyte.gz file '
+                            'downloaded from http://yann.lecun.com/exdb/mnist/')
+   parser.add_argument('--mnist-train-labels',
+                       default='train-labels-idx1-ubyte.gz',  # noqa
+                       help='Path to train-labels-idx1-ubyte.gz file '
+                            'downloaded from http://yann.lecun.com/exdb/mnist/')
+
+   args = parser.parse_args(*argument_array)
+   return args
 
 
 def main(args):
-    # Read data file into numpy matrices
-    with gzip.open(args.mnist_train_data, 'rb') as in_gzip:
-        magic, num, rows, columns = struct.unpack('>IIII', in_gzip.read(16))
-        all_data = [np.array(struct.unpack('>{}B'.format(rows * columns),
-                                           in_gzip.read(rows * columns)))
-                    for _ in range(60000)]
+   # Read data file into numpy matrices
+   with gzip.open(args.mnist_train_data, 'rb') as in_gzip:
+       magic, num, rows, columns = struct.unpack('>IIII', in_gzip.read(16))
+       all_data = [np.array(struct.unpack('>{}B'.format(rows * columns),
+                                          in_gzip.read(rows * columns)))
+                   for _ in range(60000)]
+   # Read labels file into labels
+   with gzip.open(args.mnist_train_labels, 'rb') as in_gzip:
+       magic, num = struct.unpack('>II', in_gzip.read(8))
+       all_labels = struct.unpack('>60000B', in_gzip.read(60000))
 
-    # TODO: Use Decompose into 5 components and plot the 5 components.
-    f, axarr = plt.subplots(1, 5, figsize=(18, 4), sharey=True)
-    for i in range(5):
-        # TODO: Plot each of the component in its subplot.
-        axarr[i].set_aspect('equal')
-        axarr[i].set_title('Component {}'.format(i + 1))
-    plt.tight_layout()
-    name = 'TODO'  # TODO: Remplace name with your name
-    plt.savefig('comps-{}.png'.format(name), dpi=320)
+
+
+   pca = PCA(5)
+
+
+   pca.fit(all_data)
+   components = pca.return_components()
+   components = np.reshape(components, (5, 28, 28))
+
+
+   one = PCA(5)
+   one.fit()
+   one_comp = pca.return_components()
+   f, axarr = plt.subplots(1, 5, figsize=(18, 4), sharey=True)
+
+   for i in range(5):
+       axarr[i].imshow(components[i])
+       axarr[i].set_aspect('equal')
+       axarr[i].set_title('Component {}'.format(i + 1))
+   plt.tight_layout()
+   name = 'Hrach'
+   plt.savefig('comps-{}.png'.format(name), dpi=320)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+   args = parse_args()
+
+   main(args)
