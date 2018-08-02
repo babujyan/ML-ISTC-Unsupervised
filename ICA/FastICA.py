3b5e13991fdd4a717c1c1aaf1bdc05f7eef032e8
ISTC-CREATE LAB <babujyan.hrach@gmail.com name = babujyan>
2018-08-02 16:49:02 +0400

    ICA

--- ICA/FastICA.py
+++ ICA/FastICA.py
@@ -4,19 +4,26 @@ import matplotlib.image as mpimg
 import numpy as np
 
 
-def process(images):
+def process(images,new_images, signs):
     # TODO: fix means and variances of images
-    pass
-
+    new_images = signs * new_images
+    new_images = new_images.reshape((3, 800, 600, 4))
+    return (new_images*np.std(images)/np.std(new_images)+np.mean(images))
+    
 
 if __name__ == '__main__':
+
     images = np.array([mpimg.imread(f'gen_img{i}.png').flatten() for i in range(5)])
     # TODO: Fit Fast ICA and get new images
-
     new_images = None
-    new_images = new_images.reshape((3, 800, 600, 4))
-    new_images = process(new_images)
-
-    for i in new_images:
-        plt.imshow(i.clip(min=0, max=1))
+    ica = FastICA(3)
+    new_images = ica.fit_transform(images.T).T
+    
+    signs = np.array([-1,1,1])[None].T
+    new_images = process(images, new_images, signs)
+    
+    for i, image in enumerate(new_images):
+        mpimg.imsave(f"a_{i}.png", image)
+        plt.imshow(image.clip(min=0, max=1))
         plt.show()
+    
